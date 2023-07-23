17ae7e45ad999bf0704660292fa0f24c2649c639
exceptionfactory <exceptionfactory@exceptionfactory.com>
2023-07-22 23:28:42 -0500

    Added test for X25519 Shared Secret Key Producer

---
+++ jagged-x25519/src/test/java/com/exceptionfactory/jagged/x25519/X25519SharedSecretKeyProducerTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Jagged Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exceptionfactory.jagged.x25519;
+
+import com.exceptionfactory.jagged.framework.crypto.SharedSecretKey;
+import org.junit.jupiter.api.Test;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class X25519SharedSecretKeyProducerTest {
+
+    @Test
+    void testGetSharedSecretKey() throws GeneralSecurityException {
+        final KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RecipientIndicator.KEY_ALGORITHM.getIndicator());
+
+        final KeyPair senderKeyPair = keyPairGenerator.generateKeyPair();
+        final KeyPair recipientKeyPair = keyPairGenerator.generateKeyPair();
+
+        final X25519SharedSecretKeyProducer senderProducer = new X25519SharedSecretKeyProducer(senderKeyPair.getPrivate());
+        final SharedSecretKey senderSharedSecretKey = senderProducer.getSharedSecretKey(recipientKeyPair.getPublic());
+
+        final X25519SharedSecretKeyProducer recipientProducer = new X25519SharedSecretKeyProducer(recipientKeyPair.getPrivate());
+        final SharedSecretKey recipientSharedSecretKey = recipientProducer.getSharedSecretKey(senderKeyPair.getPublic());
+
+        assertArrayEquals(senderSharedSecretKey.getEncoded(), recipientSharedSecretKey.getEncoded());
+    }
+}
